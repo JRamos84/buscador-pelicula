4c5bb95f3b4d46102421449844e9a8a2369a47ec
José <joseph0001@gmail.com>
2023-07-10 16:19:09 -0300

    seguimos

--- src/App.jsx
+++ src/App.jsx
@@ -3,37 +3,43 @@ import { useState } from 'react'
 import './App.css'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
+
+function useSearch(){
+  const [search, updateSearch] = useState()
+  const [error, setError] = useState()
+  const isFirstInput = useRef(true)
+  useEffect(()=>{
+    if(isFirstInput.current){
+      isFirstInput.current = search === ''
+      return
+    }
+    if(search === ''){
+      setError('La busqueda es al menos de 3 caracteres')
+    }
+    
+    
+    },[search])
+
+    return{search, error,updateSearch}
+}
 
 function App() {
 const {movies} = useMovies()
-const [query, setQuery] =  useState()
-const [error, setError] = useState()
-
+const {search, error, updateSearch} = useSearch()
 const handleSubmit=(event)=>{
   event.preventDefault()
-
-console.log(query)
-
-  
 }
-
 const handleChange=(event)=>{
-  setQuery(event.target.value)
-}
-useEffect(()=>{
-if(query === ''){
-  setError('La busqueda es al menos de 3 caracteres')
+  updateSearch(event.target.value)
 }
 
-
-},[query])
   return (
     <div className='page'>
       <header>
         <h1>Buscador de Películas</h1>
       <form className='form' onSubmit={handleSubmit}>
-      <input name='query' onChange={handleChange} placeholder='Avegeners, Start War, the matrix'/>
+      <input name='search' onChange={handleChange} placeholder='Avegeners, Start War, the matrix' value={search}/>
       <button type='submit' >Buscar</button>
     </form>
 {error && <p>{error}</p>}
